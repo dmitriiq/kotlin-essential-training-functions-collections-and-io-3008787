6e1e7333e39378c2747000ee8bbb509a040d90fc
Dmitrii Baikov <dmitrii.baikov@mercedes-benz.com>
2025-07-10 23:04:19 +0200

    Video 2.1, project HelloKotlin1

---
+++ project/HelloKotlin1/src/Main.kt
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var ourFirstVariable = true
+
+    println(ourFirstVariable)
+
+    ourFirstVariable = false
+    println(ourFirstVariable)
+
+    println(ourFirstVariable == true)
+    println(ourFirstVariable == false)
+}
