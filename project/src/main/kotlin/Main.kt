9762eaa78fa44594e71c4a84ae13b828419b1916
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2025-07-12 20:26:45 +0200

    Lambda function

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 
+var greetingFunction: () -> Unit = {
+    println("Hello Kitlin")
+}
+
 fun main() {
-    
+    greetingFunction()
+
+    greetingFunction = {
+        println("Hello World!")
+    }
+
+    greetingFunction.invoke()
 }
