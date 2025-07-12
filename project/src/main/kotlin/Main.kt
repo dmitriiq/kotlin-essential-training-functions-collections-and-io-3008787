2d75eca6a6b9005456914c880059338e06fd8640
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2025-07-12 21:23:46 +0200

    when, in

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,20 +1,23 @@
 
 
 fun main() {
-    var someVariable = 100
+    var someVariable = 7
 
-//    when {
-//        someVariable > 3 -> println("The value was greater than 3")
-//        someVariable > 2 -> println("The value was greater than 2")
-//        else -> {
-//            println("Not greater")
-//        }
-//    }
+    when {
+        someVariable > 3 -> println("The value was greater than 3")
+        someVariable > 2 -> println("The value was greater than 2")
+        else -> {
+            println("Not greater")
+        }
+    }
+
+    print("-----------------------------------------------\n")
 
     when (someVariable) {
         0,1 -> println("The value was 0 or 1")
         2 -> println("The value is 2")
         3 -> println("The value is 3")
-        in 4..Int.MAX_VALUE -> println("The value was greater than 3")
+//        in 4..Int.MAX_VALUE -> println("The value was greater than 3")
+        in 5..7 -> println("in 5..7")
     }
 }
