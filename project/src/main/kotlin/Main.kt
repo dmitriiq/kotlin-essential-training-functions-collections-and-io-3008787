0ba97fdfad13cef963a987f8372fbac6552595e0
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2025-07-12 20:50:31 +0200

    Higher-order function

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 
+fun printCalcVal(val1: Int, val2: Int, calc: (Int, Int) -> Int) {
+    println("The value is: ${calc(val1, val2)}")
+}
 
 fun main() {
-    
+    printCalcVal(2, 1) { value1, value2 ->
+        value1 + value2
+    }
+
+    printCalcVal(2, 1, { value1, value2 ->
+        value1 - value2
+    })
 }
