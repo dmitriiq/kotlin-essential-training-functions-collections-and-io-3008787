ad567aa702ef002ab8be1a89465eb9d911283edb
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2025-07-12 22:00:24 +0200

    Type cast

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,12 +1,21 @@
 fun checkType(input: Any?) {
+    println(input)
     when (input) {
         null -> println("Input was null")
         is String -> println("Input was a String of length ${input.length}")
     }
+
+    if (input is Long)
+        println("long")
+
+    if (input is Int)
+        println("int ")
 }
 
 fun main() {
     val aGenericVariable: Any = 5
 
     checkType("null")
+    checkType(aGenericVariable)
+    checkType(null)
 }
