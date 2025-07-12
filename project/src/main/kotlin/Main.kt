2c63f3e9bc16f41a79b5c93bee784d283a962ee1
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2025-07-12 13:57:46 +0200

    Int values

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
 
 fun main() {
-    
+    val aInt: Int = 8
+    val anotherInt = 0
+
+    val aByte: Byte = 0
+    val aShort: Short = 0
+
+
+
+    println(aInt)
+
+    println(5.9.toInt() == 5)
+    println(10.9.toFloat())
+    println((-5).toULong())
+
+    println(100_000_000.toByte())
 }
