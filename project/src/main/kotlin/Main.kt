80c8dbdcf6df6f8ee428d03a3c670fa1f9838382
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2025-07-12 21:47:31 +0200

    try catch ArithmeticException

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -2,10 +2,12 @@
 
 fun main() {
     val message = try {
-        throw IllegalStateException()
+//        throw IllegalStateException()
         "The value is ${10 / 0}"
+//    } catch (error: Throwable) {
+//        "Error Throwable $error"
     } catch (error: ArithmeticException) {
-        "Error was thrown"
+        "Error ArithmeticException '$error' was thrown"
     } catch (error: java.lang.IllegalStateException) {
         "Error was IllegalState"
     }
