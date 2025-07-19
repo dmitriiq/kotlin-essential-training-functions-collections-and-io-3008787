fbd37d8696ee4083baaf64f0bce05a5a42cdfd54
Dmitrii Baikov <dmitrii.baikov.q@gmail.com>
2025-07-19 21:49:20 +0200

    asSequence

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 
 fun main() {
-    
+    val languages = listOf("kotlin", "c++", "java").asSequence()
+
+    languages.filter{ it.length > 4 }
+        .map{ it.length }
+        .forEach { println(it) }
 }
