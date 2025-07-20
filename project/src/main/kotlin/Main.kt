dc351fae1f7febc92b32017bb57d01e491ed9d7a
Dmitrii Baikov <dmitrii.baikov.q@gmail.com>
2025-07-20 22:32:42 +0200

    asSequence

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -3,5 +3,24 @@ fun main() {
     // Using the provided test scores, identify the 3 students
     // with the lowest test scores
 
-    
+    val l = testScores
+        .map{ it.value }
+        .sorted()
+        .take(3)
+//        .forEach { println(it) }
+        .forEach { score ->
+            testScores.forEach {
+            if ( it.value == score ) {
+                println ( "key ${it.key} val ${it.value}" )
+            }
+        } }
+
+    println("------------------------------------------------------------------------------------------------------")
+
+    testScores.
+        toList()
+        .sortedBy { it.second }
+        .take(3)
+        .forEach { println("id ${it.first}, score ${it.second}") }
+
 }
