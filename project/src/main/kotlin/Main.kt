c8b7385a782188a801a4a1b18ed0074ab963b2a0
Dmitrii Baikov <dmitrii.baikov.q@gmail.com>
2025-07-20 23:14:58 +0200

    https://leetcode.com/problems/trapping-rain-water/description/
    Init loops

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,40 @@
 
-fun main() {
+class Solution {
+    fun trap(height: IntArray): Int {
+        var lastMax = 0
+        var water = 0
 
+        val size = height.size
+
+        var i = 0
+        while (i < size) {
+
+            var j = i
+            while (j < size) {
+//                println( "i=$i j=$j" )
+                if (height[j] >= lastMax) {
+                    println( "i=$i j=$j" )
+                    i = j + 1
+                    break
+                }
+                ++j
+            }
+            if (lastMax < height[i]) {
+                lastMax = height[i]
+            }
+            ++i
+        }
+
+
+        return water
+    }
+}
+
+
+fun main() {
+    val h: IntArray = intArrayOf ( 0,1,0,2,1,0,1,3,2,1,2,1 )
+
+    val s = Solution()
+
+    s.trap(h)
 }
